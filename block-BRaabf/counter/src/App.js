db1c620bd3db227d8fb82bcba7a6d1ea6360731d
anup kumar <kanup414@gmail.com>
2023-02-01 17:00:18 +0530

    done

--- block-BRaabf/counter/src/App.js
+++ block-BRaabf/counter/src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
 import "./style.css"
+
+
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             counter: 0,
+            step: 5,
+            max: 15
         }
     }
 
      handleIncreament = () => {
+        let {counter, max, step} = this.state;
         this.setState({
-            counter: this.state.counter + 1
+            counter: counter + step > max ? counter : counter + step
         })
     }
     handleDecreament = () => {
         this.setState({
-            counter: this.state.counter - 1
+            counter: this.state.counter - this.state.step
         })
     }
     reset = () => {
@@ -27,11 +32,38 @@ class App extends React.Component {
         return (
             <center>
                 <h1>{this.state.counter}</h1>
-                <h2>Steps</h2>
+                <h2 className="first">Steps</h2>
+                <h2 className="second">Max</h2>
              <div className="steps">
-                <span>05</span>
-                <span>10</span>
-                <span>15</span>
+                <>
+                
+            {[5, 10, 15].map((step) => (
+                <button
+                onClick={() => {
+                    this.setState({ step: step})
+                }}
+                className={this.state.step === step ? 'active-step' : ""}
+                >
+                    {step}
+                </button>
+            ))}
+            
+                </>
+
+                <>
+                
+            {[15, 100, 200].map((max) => (
+                <button
+                onClick={() => {
+                    this.setState({ max: max})
+                }}
+                className={this.state.max === max ? 'active-step' : ""}
+                >
+                    {max}
+                </button>
+            ))}
+                </>
+
              </div>
             <button onClick={this.handleIncreament}>Increament</button>
             <button onClick={this.handleDecreament}>Decreament</button>
