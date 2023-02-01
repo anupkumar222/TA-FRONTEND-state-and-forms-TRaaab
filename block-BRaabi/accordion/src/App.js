516bb32491b90423a896b46327df70e57b688a2b
anup kumar <kanup414@gmail.com>
2023-02-01 22:44:46 +0530

    done

--- block-BRaabi/accordion/src/App.js
+++ block-BRaabi/accordion/src/App.js
@@ -3,31 +3,35 @@ import "./style.css"
 import Questions from "./data";
 
 class App extends React.Component {
+
+
     constructor(props) {
         super(props);
+        
         this.state = {
-            Q: "JavaScript",
-            A:  'Recent college graduates, students taking gap year, dropouts, professionals looking to switch careers, anyone learning web development but needs right environment and guidance. No prior programming knowledge required.'
+            activeIndex: null,
         }
     }
 
-    open = () => {
-        Questions.map((ans, i) => {
-            <p>
-                {ans.A}
-            </p>
-        })
-    }
     render() {
         return(
+            
             <>
-            <h1>Article</h1>
+            <h1>FAQ</h1>
             <div className="container">
             <ul>
-            {Questions.map((ques) => (
-                <li className="flex color">
-                <div className="ques">{ques.Q}</div>
-                <button onClick={this.open}>📂</button>
+            {Questions.map((ques, index) => (
+ 
+                <li onClick={this.open} >
+                    <div  className={this.state.activeIndex === index ? 'active flex li' : 'flex li color'}>
+                    <div onClick={() => {this.setState({activeIndex: index})}} className="ques">{ques.Q} 
+                    </div>
+                    <div>📂</div>
+                    </div>
+                    {
+                        index === this.state.activeIndex && <p>{ques.A}</p>
+                    }
+               
              </li>
             ))}
             </ul>
