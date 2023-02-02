31e58c9a58002093e7d5f64d2424b193888fa095
anup kumar <kanup414@gmail.com>
2023-02-02 18:52:56 +0530

    done

--- block-BRaabk/food-menu/src/component/App.js
+++ block-BRaabk/food-menu/src/component/App.js
@@ -1,1 +1,21 @@
 import React from "react";
+import Header from "./Header";
+import Item from "./Item";
+import Tags from "./Tags";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <>
+            
+            <Header />
+            <Tags />
+            </>
+        )
+    }
+}
+
+export default App;
